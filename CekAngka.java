feae5dcb6d4b833baafd1e48ef4b059d42bbb949
Ahmad Zaky <zaky.ahmad849@gmail.com>
2026-06-11 08:59:42 +0700

    Add files via upload

---
+++ CekAngka.java
@@ -0,0 +1,44 @@
+/*
+Nama    : Ahmad Zaky, NIM  : 2501082008
+Desk    : 
+*/
+package praktikum11062026;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jaki
+ */
+public class CekAngka {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        int hasil;
+        try{
+        System.out.print("Masukkan bilangan pertama : ");
+        a = input.nextInt();
+        System.out.print("Masukkan bilangan kedua : ");
+        b = input.nextInt();
+        hasil = a/b;
+        System.out.println(a+" / "+b+ " = "+hasil);
+        }
+        catch(InputMismatchException e){
+            System.out.println("Masukkan angka woi bukan huruff ");
+        }
+        catch(ArithmeticException e){
+            System.out.println("\nNilai kedua atau Penyebut tidak boleh 0");
+        }
+        catch(Exception e){
+            System.out.println("Error bosku, gatau kenapa");
+        }
+        finally{
+            System.out.println("Program telah berhasil dijalankan");
+        }
+    }
+}
---
+++ CekThrow.java
@@ -0,0 +1,35 @@
+/*
+Nama    : Ahmad Zaky, NIM  : 2501082008
+Desk    : 
+*/
+package praktikum11062026;
+
+import java.util.*;
+/**
+ *
+ * @author Jaki
+ */
+public class CekThrow {
+
+    //method exception
+    public static void cekAngka(int a) throws Exception{
+        //body throw
+        if (a<0){
+            throw new Exception("Nilai tidak boleh negatif");
+        }else if (a>100){
+            throw new Exception("Nilai tidak boleh melebihi 100");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka : ");
+        int angka = input.nextInt();
+        try{
+            cekAngka(angka);
+            System.out.println("Nilai berhasil disimpan");
+        }catch(Exception e){
+            System.out.println("Terjadi eror tak terduga"+e.getMessage());
+        }
+    }
+    
+}
